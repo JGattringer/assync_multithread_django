c913c2b7f6dcea8b0e852f3dd1f263fdc53a1092
JGattringer <joaogattringer@hotmail.com>
2023-08-07 19:02:03 -0300

    assync developed
    created a scrapper with assyncaio, need to create now a Process-based parallelism and develop a django project to show up the information of the data that was scrapped from the api

---
+++ main_assync.py
@@ -0,0 +1,27 @@
+from aiohttp import ClientSession
+from asyncio import gather, get_event_loop, ensure_future
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.json()
+
+async def run():
+    tasks = []
+    async with ClientSession() as session:
+        for character_id in range(1, 827):  # Changed the range to include 1 to 827 (828 excluded)
+            task = ensure_future(
+                fetch(
+                    session,
+                    f"https://rickandmortyapi.com/api/character/{character_id}"
+                )
+            )
+            tasks.append(task)  # Append the task to the tasks list
+
+        responses = await gather(*tasks)  # Awaiting all tasks in the tasks list
+        for resp in responses:
+            print(resp["id"], resp["name"], resp["status"], resp["species"], resp["origin"]["name"], resp)
+
+
+loop = get_event_loop()
+future = ensure_future(run())
+loop.run_until_complete(future)
